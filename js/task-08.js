9a70f78b194c6802bfd6274fcb5490c00511914b
Roman Stankevych <romandionus@gmail.com>
2022-02-06 23:25:52 +0200

    task 8
    done

--- js/task-08.js
+++ js/task-08.js
@@ -8,31 +8,67 @@
 // 5. Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
 
-const form = document.querySelector(`form`)
-console.log(form)
+// const form = document.querySelector(`form`)
+// console.log(form)
 
-form.addEventListener(`submit`, onFormSubmit)
-const button = document.querySelector(`button`)
-console.dir(button)
-const input = document.querySelector(`input`)
-console.dir(input)
+// form.addEventListener(`submit`, onFormSubmit)
+// const button = document.querySelector(`button`)
+// console.dir(button)
+// const input = document.querySelector(`input`)
+// console.dir(input)
 
-function onFormSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget)
-    console.log(formData)
-    formData.forEach((value, name) => {
-        console.log(`значение свойства`, value)
-        console.log(`имя поля`, name)
-        alert(`Поля не должны быть пустыми!`)
-    })
-    
-// const formElements = event.currentTarget.elements;
-// console.dir(formElements)
+// function alert() {
+//     if (email <= 0) {
+//         alert(`Все поля должны быть заполнены!`)
+//     }
+//     else {
+//         return
+// }
+// }
 
-//     const mail = formElements.email.value;
-//     const passord = formElements.password.value;
-//     console.log(mail)
-//     console.log(passord)
+// function onFormSubmit(event) {
+//     event.preventDefault();
+//     const formData = new FormData(event.currentTarget)
+//     console.log(formData)
+
+//     // alert(`Все поля должны быть заполнены!`)
+
+
+//     formData.forEach((value, name) => {
+//         console.log(`значение свойства`, value)
+//         console.log(`имя поля`, name)
+        
+//     })
+// }
+
+// const formRef = document.querySelector('form.login-form')
+// formRef.addEventListener('submit', onFormSubmit);
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const formElements = event.currentTarget.elements;
+//   const email = formElements.email.value;
+//   const password = formElements.password.value;
+//   const formData = { email, password };
+//   if (email && password) {
+//     console.log(formData)
+//       } else {
+//       alert('Please complete all fields')
+//   }
+// }
+  
+
+const form = document.querySelector(`form`);
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+    // const email = form.email.value;
+    // const password = form.password.value;
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены!");
+  }
+console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
 }
-
