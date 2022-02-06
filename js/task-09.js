306ab9ad4a3b6e07351929d10697f369f60e8571
Roman Stankevych <romandionus@gmail.com>
2022-02-07 00:01:43 +0200

    task 9
    start

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,27 @@
+// менять цвет body через инлайн тиль по клике на кнопке
+// вывести в консоль цвет 
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnElem = document.querySelector(`button`)
+const body = document.querySelector(`body`)
+console.dir(body.style.backgroundColor)
+
+
+const clickToChange = (event) => {
+  console.log(`нажми чтобы сделать магию`, event)
+  console.log(`type`, event.type)
+  console.log(`target`, event.currentTarger)
+  console.log(`не меняет цвет`)
+}
+const wtf = btnElem.addEventListener(`click`, clickToChange);
+
+
+// if (wtf !== true) {
+// body.style.backgroundColor(grey)
+// } else {
+//   console.log(`не работает`)
+// }
